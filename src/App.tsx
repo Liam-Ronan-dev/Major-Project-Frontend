55aa9e7d508ff001ead7dda7473f97449f7c719c
Liam-Ronan-dev <liamronan16@gmail.com>
2025-01-24 20:50:06 +0000

    testing CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>testing world!</h1>
+      <h1>This is my new web application!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
