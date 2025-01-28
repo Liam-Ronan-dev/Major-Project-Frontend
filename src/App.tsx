fb477c3da1cf3f333fe9d23e3a0fa2cbae9da248
Liam-Ronan-dev <liamronan16@gmail.com>
2025-01-28 21:59:33 +0000

    testing deploy to dev S3 Bucket

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Testing deploy to prod!</h1>
+      <h1>Testing deploy to Dev!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
