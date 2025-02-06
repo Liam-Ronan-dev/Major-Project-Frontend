2238e141d823b5e52a6d7402f4bf4b940c0fad71
Liam-Ronan-dev <liamronan16@gmail.com>
2025-02-06 21:51:08 +0000

    testing husky

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    watch: false,
     globals: true, // Enables global expect() & test() functions
     environment: 'jsdom', // Simulates browser-like environment for React testing
     exclude: ['tests/E2E/**', 'node_modules', 'dist'], // Excludes Playwright tests
