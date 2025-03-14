f9328f05a6d5ee1cce017b13a9de31a3221a990f
Liam-Ronan-dev <liamronan16@gmail.com>
2025-03-14 13:30:50 +0000

    fix: updates to authContext

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { RouterProvider } from '@tanstack/react-router';
 import { ReactQueryProvider } from './lib/QueryClientProvider';
 import { router } from './router';
+import { AuthProvider } from './hooks/useAuth';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ReactQueryProvider>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </ReactQueryProvider>
   </React.StrictMode>
 );
