f2616863e4e302499f01e3d47e5436bdc43405dc
Liam-Ronan-dev <liamronan16@gmail.com>
2025-03-15 18:20:46 +0000

    feat: added tailwindcss & shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   test: {
     watch: false,
     globals: true, // Enables global expect() & test() functions
